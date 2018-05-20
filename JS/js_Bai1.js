309fcbf01704296c44990fa240ec719490c61409
Minh Thanh Pham Ngoc <minhthanh3497@gmail.com>
2018-05-20 09:55:30 +0700

    First commit

---
+++ JS/js_Bai1.js
@@ -0,0 +1,14 @@
+﻿function RandomNumber(so) {
+	var x = Math.floor((Math.random() * so) + 1)
+	document.getElementsByName("KetQua")[0].value = x;
+}
+function RandomManyNumber(so) {
+	var soLan = so;
+	var daySo = [];
+	while (soLan > 0) {
+		var x = Math.floor((Math.random() * so) + 1)
+		daySo.push(x);
+		soLan--;
+	}
+	document.getElementById('ketQua').innerHTML = daySo;
+}
